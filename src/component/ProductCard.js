e87c3878dbfc234df143b19d2ffaedba51c85235
goutham boppidi <gouthamboppidi@gmail.com>
2024-08-30 20:34:30 +0530

    ecommerce-app

---
+++ src/component/ProductCard.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({ product, addToCart }) => {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        alt={product.name}
+        height="200"
+        image={product.image}
+      />
+      <CardContent>
+        <Typography variant="h6">{product.name}</Typography>
+        <Typography variant="body2" color="textSecondary">
+          ${product.price}
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to={`/product/${product.id}`}
+          style={{ marginTop: '10px' }}
+        >
+          View Details
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => addToCart(product)}
+          style={{ marginTop: '10px', marginLeft: '10px' }}
+        >
+          Add to Cart
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
